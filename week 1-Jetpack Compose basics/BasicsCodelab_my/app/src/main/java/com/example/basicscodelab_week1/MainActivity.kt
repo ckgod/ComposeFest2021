0a375c3a3ec8eeb6accac5d8b0f9fbaf4c5f337a
ckgod <rhckdrnr123@gmail.com>
2021-11-03 19:27:06 +0900

    basics codelab step 12 complete

--- week 1-Jetpack Compose basics/BasicsCodelab_my/app/src/main/java/com/example/basicscodelab_week1/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab_my/app/src/main/java/com/example/basicscodelab_week1/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.basicscodelab_week1
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,6 +15,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basicscodelab_week1.ui.theme.BasicsCodelab_week1Theme
@@ -87,7 +89,10 @@ fun Greeting(name: String) {
                 .padding(bottom = extraPadding.coerceAtLeast(0.dp))) { // coerceAtLeast가 뭘까??
                 // 애니메이션이 진행되면서 패딩이 음수값으로 가는것을 방지함 음수값으로 가버리면 앱터짐
                 Text(text = "Hello,")
-                Text(text = "$name !")
+                Text(text = "$name !", style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                ))
+                // h1 ~ h6 , body1, body2, caption, subtitle1
             }
             OutlinedButton(
                 onClick = { expanded.value = !expanded.value }
@@ -111,10 +116,16 @@ private fun MyApp(names: List<String> = listOf("World", "Compose")) {
     }
 }
 
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     BasicsCodelab_week1Theme {
-        MyApp()
+        Greetings()
     }
 }
