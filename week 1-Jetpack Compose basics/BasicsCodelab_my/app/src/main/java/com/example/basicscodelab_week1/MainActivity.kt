756fccbb9a0bf782349e1ce97a61f7d65e4def1e
ckgod <rhckdrnr123@gmail.com>
2021-11-03 19:56:51 +0900

    basics codelab step 13 complete

--- week 1-Jetpack Compose basics/BasicsCodelab_my/app/src/main/java/com/example/basicscodelab_week1/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab_my/app/src/main/java/com/example/basicscodelab_week1/MainActivity.kt
@@ -4,17 +4,27 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons.Filled
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -49,13 +59,7 @@ private fun OnboardingScreen(onContinueClicked: () -> Unit) {
     }
 }
 
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
-@Composable
-fun OnBoardingPreview() {
-    BasicsCodelab_week1Theme {
-        OnboardingScreen(onContinueClicked = {})
-    }
-}
+
 
 @Composable
 private fun Greetings(names : List<String> = List(1000) {"$it"}) {
@@ -68,44 +72,69 @@ private fun Greetings(names : List<String> = List(1000) {"$it"}) {
 
 @Composable // 컴포저블 함수로 인식
 fun Greeting(name: String) {
-    // 여기에 rememberSaveable을 사용하면 아래로 스크롤하고 다시 위로올려도 확장된 상태가 유지된다.
-    val expanded = remember { mutableStateOf(false) }
-
-    val extraPadding by animateDpAsState(
-        if(expanded.value) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
-
-    Surface(
-        color = MaterialTheme.colors.primary,
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding.coerceAtLeast(0.dp))) { // coerceAtLeast가 뭘까??
-                // 애니메이션이 진행되면서 패딩이 음수값으로 가는것을 방지함 음수값으로 가버리면 앱터짐
-                Text(text = "Hello,")
-                Text(text = "$name !", style = MaterialTheme.typography.h4.copy(
-                    fontWeight = FontWeight.ExtraBold
-                ))
-                // h1 ~ h6 , body1, body2, caption, subtitle1
-            }
-            OutlinedButton(
-                onClick = { expanded.value = !expanded.value }
-            ) {
-                Text(if (expanded.value) "Show less" else "Show more")
-            }
-        }
-
+        CardContent(name = name)
     }
 }
 
 @Composable
-private fun MyApp(names: List<String> = listOf("World", "Compose")) {
+private fun CardContent(name: String) {
+    // 여기에 rememberSaveable을 사용하면 아래로 스크롤하고 다시 위로올려도 확장된 상태가 유지된다.
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioLowBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        // coerceAtLeast가 뭘까??
+        // 애니메이션이 진행되면서 패딩이 음수값으로 가는것을 방지함 음수값으로 가버리면 앱터짐
+        ) {
+            Text(text = "Hello,")
+            Text(
+                text = "$name",
+                style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+                // h1 ~ h6 , body1, body2, caption, subtitle1
+            )
+            if (expanded) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy").repeat(4),
+                )
+            }
+
+        }
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Filled.ExpandLess else Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+
+            )
+        }
+    }
+
+}
+
+@Composable
+private fun MyApp() {
     var shouldShowingOnboarding by rememberSaveable {
         mutableStateOf(true)
     }
@@ -113,6 +142,14 @@ private fun MyApp(names: List<String> = listOf("World", "Compose")) {
         OnboardingScreen(onContinueClicked = {shouldShowingOnboarding = false})
     } else {
         Greetings()
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnBoardingPreview() {
+    BasicsCodelab_week1Theme {
+        OnboardingScreen(onContinueClicked = {})
     }
 }
 
